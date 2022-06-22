761b2b33522ab482e86c13a631351a36473c9519
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-22 14:21:43 +0200

    exo 9

---
+++ index.js
@@ -0,0 +1,87 @@
+// exo1
+
+// const fruits = ["mongo", "lemon", "blueberry"]
+// console.log(fruits);
+// console.table(fruits);
+
+// exo2
+// const ingredients = ["eggs", "milk", "butter"]
+// console.log(ingredients[1]);
+// console.log(ingredients.indexOf("butter"));
+
+// exo3
+
+// const objects = ["pen", "book", "lamp"]
+// objects.unshift("chair")
+// console.log(objects);
+// objects.pop()
+// console.log(objects);
+// objects.push("laptop")
+// console.log(objects);
+// objects.shift()
+// console.log(objects);
+
+// exo4
+
+// const numbers = [4, 10, 8, 12, 6]
+// numbers.reverse()
+// console.log(numbers);
+
+// exo5
+
+// let total = 0
+// const limit = 10
+
+// for (let i = 0; i <= limit; i ++) {
+//     total = total+ i
+//     console.log(total);
+// }
+
+// exo6
+
+// const sentence = "Hello Konexio !"
+
+// for (let i = sentence.length-1; i >= 0; i--) {
+
+//     console.log(sentence.charAt(i)); 
+// }
+
+// exo7
+
+// for (let i = 0; i <= 100; i++) {
+    
+//     if (i % 7 === 0 ) {
+//         console.log("");
+//     } else if (i % 3 === 0 && i % 5 === 0){
+//         console.log("fizzbuzz")
+//     } else if (i % 5 === 0 ) {
+//         console.log("buzz");
+//     } else if (i % 3 === 0 ) {
+//         console.log("fizz");
+//     } else {
+//         console.log(i);
+//     }      
+// }
+
+// exo8
+
+// let counter = 0
+// const count = 100
+// while (counter < count) {
+//     counter++
+//         if (counter % 7 === 0 ) {
+//         console.log("");
+//     } else if (counter % 3 === 0 && counter % 5 === 0){
+//         console.log("fizzbuzz")
+//     } else if (counter % 5 === 0 ) {
+//         console.log("buzz");
+//     } else if (counter % 3 === 0 ) {
+//         console.log("fizz");
+//     } else {
+//         console.log(counter);
+//     }   
+// }
+
+// exo9
+
+const students = ["Walid","Wade","Rayan","Walid","Benoit","Emad","Jad","Lynda","Florez","Lucas","Arthur","Thomas","Aurelien","Clara","Danh","Cinie","Edaly","Mago","Dushen","Julien","Florent"];
